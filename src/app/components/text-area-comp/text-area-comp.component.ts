fbf0f54664d9c175c65459dcafffe8b0ea0de361
Chandra Sekhar Kumar Pasagadugula <ienthusiastic.chandra@outlook.com>
2023-06-23 22:39:41 +0530

    textare and textboc changes

--- src/app/components/text-area-comp/text-area-comp.component.ts
+++ src/app/components/text-area-comp/text-area-comp.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { DataCollectionItemList, ResponseItem } from 'src/app/survey-form/survery-form.component.model';
 
 @Component({
@@ -16,7 +16,7 @@ export class TextAreaCompComponent implements OnChanges {
   textAreaRows: number | null = null;
   textAreaMaxlength: number | null = null
   TextType: string = 'TEXTAREA';
-  
+
 
   ngOnChanges(changes: SimpleChanges): void {
 
@@ -27,13 +27,13 @@ export class TextAreaCompComponent implements OnChanges {
       this.textAreaContentData = contentData;
       this.textAreaRows = this.textAreaContentData.nbr_row;
       this.textAreaMaxlength = this.textAreaContentData.nbr_char;
-      this.TextType =  this.textAreaContentData.cd_srvy_item_type === 'NOTE' ? 'TEXTAREA' : 'TEXT';
+      this.TextType = this.textAreaContentData.cd_srvy_item_type === 'NOTE' ? 'TEXTAREA' : 'TEXT';
 
       if (contentData.response_items != null && contentData.response_items != undefined) {
         this.textAreaResponseContent = contentData.response_items.at(0);
 
         if (this.textAreaResponseContent != null && this.textAreaResponseContent.txt_rspns_client != null && this.textAreaResponseContent.txt_rspns_client != undefined) {
-          this.textAreatText = this.textAreaResponseContent.txt_rspns_client;         
+          this.textAreatText = this.textAreaResponseContent.txt_rspns_client;
         }
       }
     }
@@ -41,4 +41,19 @@ export class TextAreaCompComponent implements OnChanges {
 
   @Input()
   TextAreaDataItem: DataCollectionItemList | undefined;
+
+  @Output()
+  OnComponentDataChange: EventEmitter<DataCollectionItemList> = new EventEmitter<DataCollectionItemList>();
+
+  OnTextDataChange(data: any) {
+    if (this.TextAreaDataItem != null && this.TextAreaDataItem != undefined) {
+      if (this.TextAreaDataItem.response_items != null && this.TextAreaDataItem.response_items != undefined && this.TextAreaDataItem.response_items.length > 0) {
+        this.TextAreaDataItem.response_items[0].txt_rspns_client = data;
+        this.OnComponentDataChange.emit(this.TextAreaDataItem)
+      }
+    }
+    console.log(data);
+  }
+
+
 }
